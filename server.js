8c173a2af0ed43209fbbfdddd49af1259bf4e3cd
Learnwave <146640293+Learnwave@users.noreply.github.com>
2024-12-23 19:26:36 +0530

    database setup

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDb from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+
 
 //app config
+
+
 
 const app = express();
 
 const port = process.env.PORT || 4000
 
+connectDb()
+connectCloudinary()
 //middle wares
 
 app.use(express.json());
